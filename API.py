83e0a57e6467eb5c31492b1773a727f2dfdf607e
Tshegofatso Sekgothe <tsekgothe023@student.wethinkcode.co.za>
2024-10-04 10:40:09 +0200

    mic starts listening once question has been read

--- API.py
+++ API.py
@@ -3,6 +3,7 @@ import os
 import time
 import speech_recognition as sr
 from gtts import gTTS
+from pydub import AudioSegment
 
 
 api_key = os.getenv('GENAI_API_KEY')
@@ -12,7 +13,7 @@ if not api_key:
 genai.configure(api_key=os.environ['GENAI_API_KEY'])
  
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("You are an iterviewer. You are interviewing a candidate for a job. The candidate is a software engineer. please evalute their performance. Give 5 questions")
+response = model.generate_content("You are an iterviewer. You are interviewing a candidate for a job. The candidate is a software engineer. please evalute their performance.")
 # print(response.text)
 
 while not response.text:
@@ -25,7 +26,7 @@ type_of_interview = "Behavioral"
 model.generate_content("At the end give me an overall impression of the candidate's performance")
 
 response = model.generate_content("1. " + field_of_study + "\n2. " + type_of_interview + " Interview Questions" + "\n3. " + proficiency + " Candidate")
-# print(response.text)
+print(response.text)
 
 questions = response.text.split("* **")
 # print(questions)
@@ -41,16 +42,22 @@ for question in questions:
         # print(question)
         tts = gTTS(question, lang='en')
         tts.save("question.mp3")
-        os.system("start question.mp3")
+        os.system("mpg123 question.mp3")
+
+        audio_file = AudioSegment.from_file("/home/wethinkcode_/student_work/personal_prodj/PrepMate-1/question.mp3")
+        audio_duration = len(audio_file) / 1000
         try:
-            time.sleep(20)
+            # time.sleep(audio_duration)
+            print("Listening...")
             audio = recognizer.listen(source)
             answer = recognizer.recognize_google(audio)
             candidate_answers[question] = answer
+            print(candidate_answers) 
         except sr.UnknownValueError:
             comp = "Sorry, I did not get that"
             tts = gTTS(comp, lang='en')
             tts.save("comp.mp3")
-            os.system("start comp.mp3")
+            os.system("mpg123 comp.mp3")
+            print(candidate_answers) 
 
-print(candidate_answers)       # print(question)
+print(candidate_answers)
